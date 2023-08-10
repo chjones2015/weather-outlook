86350c15690b126d91f663450736766bf09818fa
Christian Jones <chjones2015@gmail.com>
2023-08-09 21:59:27 -0500

    added some js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const apiKey = 'your-api-key';
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const currentWeather = document.querySelector('#current-weather');
+const forecast = document.querySelector('#forecast');
+const searchHistory = document.querySelector('#search-history');
